74238a71323697a145d9712abb1e3f780ad32e57
MasonUlferts <815467@mystma.org>
2020-04-08 16:46:26 +0000

    added top five movies

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Creed");
+        System.out.println("2. star wars");
+        System.out.println("3. black panther");
+        System.out.println("4. end game");
+        System.out.println("5. knives out");
     }
 }
