b373faba17ba799fdbe63299ca167574d97eff84
MasonUlferts <815467@mystma.org>
2020-04-14 16:31:09 +0000

    color

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,10 +1,12 @@
 package app;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-     String myname = "mason ulferts";
-     System.out.println(myname);
-    String madlib = "the dog was pet by ___ this dogs name was ____ he is an ___ and is very ____";
-    System.out.println(madlib);
+   String color = "blue";
+while (color == "Blue");
+System.out.println("the color is " + color);
+color = "green";
     }
 }
