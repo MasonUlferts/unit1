a85122b3bf6cddefaddc44cbe465500f9e7b1259
MasonUlferts <815467@mystma.org>
2020-04-16 17:03:25 +0000

    for loop assignment i have no clue what im doing

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -4,8 +4,9 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-  for (int i = 0; i <= 10; i = i + 1){
- System.out.println("the count is " + i);  
-System.out.println("let's keep counting."); 
+  
+for (int i = 0; i <= 81; i = i + 1);
+String i;
+System.out.println("the count is" + i);
 }
-}}
+}
