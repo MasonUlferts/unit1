7f34c4a26b5b85ef7a07140ba5bea2ca27d5bcd1
MasonUlferts <815467@mystma.org>
2020-04-17 15:49:33 +0000

    for loop assignment

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,8 +5,23 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
   
-for (int i = 0; i <= 81; i = i + 1);
-String i;
-System.out.println("the count is" + i);
+        for (int i = 22; i <=28; i = i + 3)
+        System.out.println(i);
+
+        for (int i = 2; i <= 32; i = i * 2)
+        System.out.println(i);
+
+        for (int i = 3; i >= -81; i = i * -i)
+        System.out.println(i);
+
+        for (int i = 50;  i >= 5; i = i - 5)
+        System.out.println(i);
+
+        for (int i = 1; i <= 9; i = i + 1){
+        System.out.println(i * i);
+        System.out.println(i + 3);
+        System.out.println(i);
+        }
+
 }
 }
