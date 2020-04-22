41fdcf4c7a2131b19d7d00364af6d2dfc909735c
MasonUlferts <815467@mystma.org>
2020-04-22 16:05:07 +0000

    new for loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -3,15 +3,30 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
   
-      for (int i = 1; i <= 20; i++){
-          if (i % 2 == 0) {
-              System.out.println(i + " is an even number");
-          } else {
-              System.out.println(i + "is an odd number");
-          }
-          System.out.println("all finished");
-      } 
-        
-        }
+     System.out.println("start first loop");
 
-}
+     for (int i = 0; i < 20; i++){
+         System.out.println(i);
+        if (i == 10){
+            for (int j = 0; j < 5; j++){
+                System.out.println(i);
+            }
+        }
+    }   
+                
+               System.out.println("end first loop");
+               System.out.println("start second loop");
+               for (int i = 0; i < 20; i = i + 2){
+                   if (i % 3 == 0){
+                       i = i - 1;
+                    }
+                    System.out.println(i);
+               }
+            
+        }
+     }
+       
+        
+        
+    
+
