8ac1fa6aa08eb04df8c7c8c8ecdba5a55c02c89f
MasonUlferts <815467@mystma.org>
2020-04-23 16:11:30 +0000

    it wont let me do i twice

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -3,28 +3,29 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
   
-     System.out.println("start first loop");
+        System.out.println("start first loop");
+        for (int i = 20; i > 0; i = i - 1){
+            System.out.println(i);
+            if (i == 10){
+                for (int j = 0; j < 7; j++){
+                    System.out.println(i);{
 
-     for (int i = 0; i < 20; i++){
-         System.out.println(i);
-        if (i == 10){
-            for (int j = 0; j < 5; j++){
-                System.out.println(i);
+                    }
+                }
+            }
+            System.out.println("end of first loop");
+            System.out.println("start second loop");{
+            for (int g = 0; g < 37; g = g + 4){
+                System.out.println(g);
             }
         }
-    }   
-                
-               System.out.println("end first loop");
-               System.out.println("start second loop");
-               for (int i = 0; i < 20; i = i + 2){
-                   if (i % 3 == 0){
-                       i = i - 1;
-                    }
-                    System.out.println(i);
-               }
+    }
+}}
+        
+               
             
-        }
-     }
+        
+     
        
         
         
