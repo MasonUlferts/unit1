dffc24135e48c1f2754d0f819dc59b9986d3342c
MasonUlferts <815467@mystma.org>
2020-04-24 15:40:45 +0000

    quiz

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -4,23 +4,29 @@ public class App {
     public static void main(String[] args) throws Exception {
   
         System.out.println("start first loop");
-        for (int i = 20; i > 0; i = i - 1){
-            System.out.println(i);
-            if (i == 10){
-                for (int j = 0; j < 7; j++){
+        for (int i = 1; i < 11; i = i + 1){
+            System.out.println(i);{
+             
+            }
+          
+
+            }
+            System.out.println("end first loop");
+            System.out.println("start second loop");{
+                for (int i = 100; i <= 150; i = i + 10){
                     System.out.println(i);{
 
                     }
+                  
+                    }
+                    System.out.println("end second loop");
                 }
             }
-            System.out.println("end of first loop");
-            System.out.println("start second loop");{
-            for (int g = 0; g < 37; g = g + 4){
-                System.out.println(g);
-            }
+
         }
-    }
-}}
+    
+    
+
         
                
             
