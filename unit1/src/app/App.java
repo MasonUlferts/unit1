4ef0a16b0a90cf116a38acdc528c58521818a284
MasonUlferts <815467@mystma.org>
2020-04-28 16:55:08 +0000

    scanner

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,39 +1,16 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-  
-        System.out.println("start first loop");
-        for (int i = 1; i < 11; i = i + 1){
-            System.out.println(i);{
-             
-            }
-          
-
-            }
-            System.out.println("end first loop");
-            System.out.println("start second loop");{
-                for (int i = 100; i <= 150; i = i + 10){
-                    System.out.println(i);{
-
-                    }
-                  
-                    }
-                    System.out.println("end second loop");
-                }
-            }
-
-        }
-    
-    
-
-        
-               
-            
-        
-     
+ 
+        Scanner input = new Scanner(System.in);
        
-        
-        
-    
+        System.out.println("please enter your first name.");
+        String name = input.nextLine();
 
+        System.out.println("hello, " + name);
+      
+    }
+ }
