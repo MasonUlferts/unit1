e96b2bdf59c1d0f81b0324d41d2a49330e6bc519
MasonUlferts <815467@mystma.org>
2020-05-05 16:09:25 +0000

    make your own quiz

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,25 +1,41 @@
 package app;
 
-
+import java.util.Scanner;
 
 public class App {
+  
     public static void main(String[] args) throws Exception {
-        int count = 0;
-        String color ="red";
+        Scanner input = new Scanner(System.in);
 
-        while (count < 10){
-        System.out.println("the count is " + count);{
+        System.out.println("how many inches are in a foot");
+        String enteredpassword = input.nextLine();
+
+        if (enteredpassword.equals(12)){
+          
+        }
+        System.out.println("CORRECT");{
 
         }
-        count = count + 1;
-        }
-        System.out.println("count finished");
+        System.out.println("What is the capitol of Minnesota");
+        Scanner capitol = new Scanner(System.in);
 
-        while (color == "red");{
-            System.out.println("the color is" + color);
-            color = "green";
+        String password = capitol.nextLine();
+
+        if (password.equals("Minneapolis")){
+
         }
-        System.out.println("color finished");
+        System.out.println("Incorrect");{
+
+        }
+        System.out.println("what year is it");
+        Scanner year = new Scanner(System.in);
+
+        String newpassword = year.nextLine();
+
+        if (newpassword.equals(2020)){
+
+        }
+        System.out.println("CORRECT");
 
 
         
