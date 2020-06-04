da40edbd7c3c70e2d041d7234004cee9bc081fe9
MasonUlferts <815467@mystma.org>
2020-06-04 18:45:48 +0000

    final

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,14 +5,30 @@ import java.util.Scanner;
 public class App {
   
     public static void main(String[] args) throws Exception {
-        
-      Person person1 = new Person();
-      Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
+       Account account1 = new Account();
 
-      person1.name = "Bob";
+       System.out.println("what is your name");
+       account1.name = input.nextLine();
 
-      System.out.println(person1.name);
+       System.out.println("what is your starting balence");
+       account1.startingbalance = input.nextLine();
+
+       System.out.println("What passcode do you want");
+       account1.password = input.nextLine();
+
+       System.out.println("Account created, please log in.");
+
+       System.out.println("what is your name?");
+       input.nextLine();
+       String name1 = input.nextLine();
+       If (name1.equals(account.name)) {
+           System.out.println("enter your password");
+           int password1 = input.nextLine();
+       }
     }
+
+    
  
 
 
--- unit1/src/app/Dog.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class Dog{
-
-    String Name;
-    int breed;
-    String age;
-}
